8dd161d1ba6640b6b86126fa624ac71590612914
Applzor <applzor@hotmail.com>
2013-09-10 16:32:31 +1000

    First Commit

---
+++ SpriteMap/Main.Designer.cs
@@ -0,0 +1,397 @@
+﻿namespace SpriteMap
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.msMain_File = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsFile_New = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsFile_Open = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsFile_Seperator0 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsFile_Save = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsFile_SaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsFile_Seperator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsFile_Export = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsFile_ExportAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsFile_Seperator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsFile_Quit = new System.Windows.Forms.ToolStripMenuItem();
+            this.msMain_Edit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsEdit_AddSprites = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsEdit_AddFolder = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsEdit_Delete = new System.Windows.Forms.ToolStripMenuItem();
+            this.msMain_View = new System.Windows.Forms.ToolStripMenuItem();
+            this.msMain_Tools = new System.Windows.Forms.ToolStripMenuItem();
+            this.msMain_Help = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.msMain = new System.Windows.Forms.MenuStrip();
+            this.lbSprites = new System.Windows.Forms.ListBox();
+            this.pbSpriteSheet = new System.Windows.Forms.PictureBox();
+            this.pbPreview = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAddSprites = new System.Windows.Forms.Button();
+            this.btnAddFolder = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.msMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSpriteSheet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // msMain_File
+            // 
+            this.msMain_File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsFile_New,
+            this.tsFile_Open,
+            this.tsFile_Seperator0,
+            this.tsFile_Save,
+            this.tsFile_SaveAs,
+            this.tsFile_Seperator1,
+            this.tsFile_Export,
+            this.tsFile_ExportAs,
+            this.tsFile_Seperator2,
+            this.tsFile_Quit});
+            this.msMain_File.Name = "msMain_File";
+            this.msMain_File.Size = new System.Drawing.Size(37, 20);
+            this.msMain_File.Text = "File";
+            // 
+            // tsFile_New
+            // 
+            this.tsFile_New.Name = "tsFile_New";
+            this.tsFile_New.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.tsFile_New.Size = new System.Drawing.Size(204, 22);
+            this.tsFile_New.Text = "New";
+            // 
+            // tsFile_Open
+            // 
+            this.tsFile_Open.Name = "tsFile_Open";
+            this.tsFile_Open.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.tsFile_Open.Size = new System.Drawing.Size(204, 22);
+            this.tsFile_Open.Text = "Open";
+            // 
+            // tsFile_Seperator0
+            // 
+            this.tsFile_Seperator0.Name = "tsFile_Seperator0";
+            this.tsFile_Seperator0.Size = new System.Drawing.Size(201, 6);
+            // 
+            // tsFile_Save
+            // 
+            this.tsFile_Save.Name = "tsFile_Save";
+            this.tsFile_Save.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.tsFile_Save.Size = new System.Drawing.Size(204, 22);
+            this.tsFile_Save.Text = "Save";
+            // 
+            // tsFile_SaveAs
+            // 
+            this.tsFile_SaveAs.Name = "tsFile_SaveAs";
+            this.tsFile_SaveAs.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.tsFile_SaveAs.Size = new System.Drawing.Size(204, 22);
+            this.tsFile_SaveAs.Text = "Save As...";
+            // 
+            // tsFile_Seperator1
+            // 
+            this.tsFile_Seperator1.Name = "tsFile_Seperator1";
+            this.tsFile_Seperator1.Size = new System.Drawing.Size(201, 6);
+            // 
+            // tsFile_Export
+            // 
+            this.tsFile_Export.Name = "tsFile_Export";
+            this.tsFile_Export.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.tsFile_Export.Size = new System.Drawing.Size(204, 22);
+            this.tsFile_Export.Text = "Export";
+            // 
+            // tsFile_ExportAs
+            // 
+            this.tsFile_ExportAs.Name = "tsFile_ExportAs";
+            this.tsFile_ExportAs.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.E)));
+            this.tsFile_ExportAs.Size = new System.Drawing.Size(204, 22);
+            this.tsFile_ExportAs.Text = "Export As...";
+            // 
+            // tsFile_Seperator2
+            // 
+            this.tsFile_Seperator2.Name = "tsFile_Seperator2";
+            this.tsFile_Seperator2.Size = new System.Drawing.Size(201, 6);
+            // 
+            // tsFile_Quit
+            // 
+            this.tsFile_Quit.Name = "tsFile_Quit";
+            this.tsFile_Quit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.tsFile_Quit.Size = new System.Drawing.Size(204, 22);
+            this.tsFile_Quit.Text = "Quit";
+            // 
+            // msMain_Edit
+            // 
+            this.msMain_Edit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsEdit_AddSprites,
+            this.tsEdit_AddFolder,
+            this.tsEdit_Delete});
+            this.msMain_Edit.Name = "msMain_Edit";
+            this.msMain_Edit.Size = new System.Drawing.Size(39, 20);
+            this.msMain_Edit.Text = "Edit";
+            // 
+            // tsEdit_AddSprites
+            // 
+            this.tsEdit_AddSprites.Name = "tsEdit_AddSprites";
+            this.tsEdit_AddSprites.Size = new System.Drawing.Size(134, 22);
+            this.tsEdit_AddSprites.Text = "Add Sprites";
+            this.tsEdit_AddSprites.Click += new System.EventHandler(this.tsEdit_AddSprites_Click);
+            // 
+            // tsEdit_AddFolder
+            // 
+            this.tsEdit_AddFolder.Name = "tsEdit_AddFolder";
+            this.tsEdit_AddFolder.Size = new System.Drawing.Size(134, 22);
+            this.tsEdit_AddFolder.Text = "Add Folder";
+            // 
+            // tsEdit_Delete
+            // 
+            this.tsEdit_Delete.Name = "tsEdit_Delete";
+            this.tsEdit_Delete.Size = new System.Drawing.Size(134, 22);
+            this.tsEdit_Delete.Text = "Delete";
+            // 
+            // msMain_View
+            // 
+            this.msMain_View.Name = "msMain_View";
+            this.msMain_View.Size = new System.Drawing.Size(44, 20);
+            this.msMain_View.Text = "View";
+            // 
+            // msMain_Tools
+            // 
+            this.msMain_Tools.Name = "msMain_Tools";
+            this.msMain_Tools.Size = new System.Drawing.Size(48, 20);
+            this.msMain_Tools.Text = "Tools";
+            // 
+            // msMain_Help
+            // 
+            this.msMain_Help.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.msMain_Help.Name = "msMain_Help";
+            this.msMain_Help.Size = new System.Drawing.Size(44, 20);
+            this.msMain_Help.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // msMain
+            // 
+            this.msMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.msMain_File,
+            this.msMain_Edit,
+            this.msMain_View,
+            this.msMain_Tools,
+            this.msMain_Help});
+            this.msMain.Location = new System.Drawing.Point(0, 0);
+            this.msMain.Name = "msMain";
+            this.msMain.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.msMain.Size = new System.Drawing.Size(784, 24);
+            this.msMain.TabIndex = 0;
+            this.msMain.Text = "MainMenuStrip";
+            // 
+            // lbSprites
+            // 
+            this.lbSprites.FormattingEnabled = true;
+            this.lbSprites.Location = new System.Drawing.Point(12, 95);
+            this.lbSprites.Name = "lbSprites";
+            this.lbSprites.Size = new System.Drawing.Size(150, 277);
+            this.lbSprites.TabIndex = 3;
+            // 
+            // pbSpriteSheet
+            // 
+            this.pbSpriteSheet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbSpriteSheet.Location = new System.Drawing.Point(168, 95);
+            this.pbSpriteSheet.Name = "pbSpriteSheet";
+            this.pbSpriteSheet.Size = new System.Drawing.Size(604, 455);
+            this.pbSpriteSheet.TabIndex = 2;
+            this.pbSpriteSheet.TabStop = false;
+            // 
+            // pbPreview
+            // 
+            this.pbPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbPreview.Location = new System.Drawing.Point(12, 400);
+            this.pbPreview.Name = "pbPreview";
+            this.pbPreview.Size = new System.Drawing.Size(150, 150);
+            this.pbPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPreview.TabIndex = 1;
+            this.pbPreview.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 80);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Sprite Sheet";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 384);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Preview";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sprites";
+            // 
+            // btnAddSprites
+            // 
+            this.btnAddSprites.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnAddSprites.Location = new System.Drawing.Point(180, 27);
+            this.btnAddSprites.Name = "btnAddSprites";
+            this.btnAddSprites.Size = new System.Drawing.Size(75, 50);
+            this.btnAddSprites.TabIndex = 7;
+            this.btnAddSprites.Text = "Add Sprites";
+            this.btnAddSprites.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAddSprites.UseVisualStyleBackColor = true;
+            this.btnAddSprites.Click += new System.EventHandler(this.btnAddSprites_Click);
+            // 
+            // btnAddFolder
+            // 
+            this.btnAddFolder.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnAddFolder.Location = new System.Drawing.Point(261, 27);
+            this.btnAddFolder.Name = "btnAddFolder";
+            this.btnAddFolder.Size = new System.Drawing.Size(75, 50);
+            this.btnAddFolder.TabIndex = 8;
+            this.btnAddFolder.Text = "Add Folder";
+            this.btnAddFolder.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAddFolder.UseVisualStyleBackColor = true;
+            // 
+            // btnNew
+            // 
+            this.btnNew.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnNew.Location = new System.Drawing.Point(12, 27);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(50, 50);
+            this.btnNew.TabIndex = 9;
+            this.btnNew.Text = "New";
+            this.btnNew.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnOpen.Location = new System.Drawing.Point(68, 27);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(50, 50);
+            this.btnOpen.TabIndex = 10;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnOpen.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSave.Location = new System.Drawing.Point(124, 27);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(50, 50);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Save";
+            this.btnSave.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnAddFolder);
+            this.Controls.Add(this.btnAddSprites);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbSprites);
+            this.Controls.Add(this.pbSpriteSheet);
+            this.Controls.Add(this.pbPreview);
+            this.Controls.Add(this.msMain);
+            this.MainMenuStrip = this.msMain;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.msMain.ResumeLayout(false);
+            this.msMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSpriteSheet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbPreview;
+        private System.Windows.Forms.ToolStripMenuItem msMain_File;
+        private System.Windows.Forms.ToolStripMenuItem tsFile_New;
+        private System.Windows.Forms.ToolStripMenuItem tsFile_Open;
+        private System.Windows.Forms.ToolStripSeparator tsFile_Seperator0;
+        private System.Windows.Forms.ToolStripMenuItem tsFile_Save;
+        private System.Windows.Forms.ToolStripMenuItem tsFile_SaveAs;
+        private System.Windows.Forms.ToolStripSeparator tsFile_Seperator1;
+        private System.Windows.Forms.ToolStripMenuItem tsFile_Export;
+        private System.Windows.Forms.ToolStripMenuItem tsFile_ExportAs;
+        private System.Windows.Forms.ToolStripSeparator tsFile_Seperator2;
+        private System.Windows.Forms.ToolStripMenuItem tsFile_Quit;
+        private System.Windows.Forms.ToolStripMenuItem msMain_Edit;
+        private System.Windows.Forms.ToolStripMenuItem tsEdit_AddSprites;
+        private System.Windows.Forms.ToolStripMenuItem tsEdit_AddFolder;
+        private System.Windows.Forms.ToolStripMenuItem tsEdit_Delete;
+        private System.Windows.Forms.ToolStripMenuItem msMain_View;
+        private System.Windows.Forms.ToolStripMenuItem msMain_Tools;
+        private System.Windows.Forms.ToolStripMenuItem msMain_Help;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip msMain;
+        private System.Windows.Forms.ListBox lbSprites;
+        private System.Windows.Forms.PictureBox pbSpriteSheet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAddSprites;
+        private System.Windows.Forms.Button btnAddFolder;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+
+    }
+}
---
+++ SpriteMap/Main.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace SpriteMap
+{
+    public partial class Main : Form
+    {
+        List<String> sprites = new List<String>();
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void tsEdit_AddSprites_Click(object sender, EventArgs e)
+        {
+            string filepath = "";
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png|All files (*.*)|*.*";
+            ofd.Multiselect = true;
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                foreach (String file in ofd.FileNames)
+                {
+                    filepath = file;
+                    pbPreview.Load(filepath);
+                    if (pbPreview.Image.Width <= pbPreview.Width && pbPreview.Image.Height <= pbPreview.Height)
+                        pbPreview.SizeMode = PictureBoxSizeMode.CenterImage;
+                    else
+                        pbPreview.SizeMode = PictureBoxSizeMode.Zoom;
+                    sprites.Add(filepath);
+
+                    
+                    int offset = file.LastIndexOf("\\");
+                    filepath = file.Substring(offset + 1, file.Length - offset - 1);
+                    lbSprites.Items.Add(filepath);
+                }                
+            }            
+        }
+
+        private void btnAddSprites_Click(object sender, EventArgs e)
+        {
+            tsEdit_AddSprites_Click(sender, e);
+        }
+    }
+}
